0a0b78f1db3f29b3af77294f616f992b540dcc9e
alistair-mclean <alistair.i.mclean@gmail.com>
2017-06-23 14:16:59 -0600

    Beginning first stages of developing the display scheme
    + Creating a test display canvas to play around with
    + Created a Velocity Display Chunk, which will soon display the Velocity of a selected object
    + Creating a script to act as a 'valve' of invormation to the canvas. Controlling what is shown and what isn't.

---
+++ Virtual Laboratory/Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour {
+  // DESCRIPTION - Resets the gameobject to the original location 
+  // once it has gone below a certain height. With the ability to
+  // toggle the resetting of forces or not. Defaulted to yes. 
+  // As a reminder, the y-direction is considered to be the vertical direction in all scenes!
+
+  //Public
+  public Vector3 MinimumHeight;
+  public bool ResetVelocity = true;
+
+  //Private
+  private Rigidbody _object;
+  private Vector3 _initialPosition = new Vector3(0.0f, 0.0f, 0.0f);
+
+  private void Start()
+  {
+    _object = GetComponent<Rigidbody>();
+    _initialPosition = transform.position;
+  }
+
+  void LateUpdate () {
+    if (transform.position.y <= MinimumHeight.y) { 
+      transform.position = _initialPosition;
+      if (ResetVelocity) { 
+        _object.velocity = Vector3.zero;
+        _object.angularVelocity = Vector3.zero;
+      }
+    }
+  }
+}
---
+++ Virtual Laboratory/Assets/Scripts/UserControls.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControls : MonoBehaviour {
+  
+  // I need to make this script activate the ball object once clicked, and 
+  // start sending information to the display canvas
+  // 
+  //
+}
