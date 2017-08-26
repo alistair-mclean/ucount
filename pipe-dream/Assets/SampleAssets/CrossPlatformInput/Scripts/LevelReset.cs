8fa59f93b7f034cd8f474888efefcb7a0b58693d
alistair-mclean <alistair.i.mclean@gmail.com>
2017-08-26 13:04:49 -0600

    Adding a map marker, integrating AR features

---
+++ pipe-dream/Assets/SampleAssets/CrossPlatformInput/Scripts/LevelReset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour , IPointerClickHandler
+{
+
+	public void OnPointerClick (PointerEventData data) {
+
+        // reload the scene
+        // Application.LoadLevelAsync(Application.loadedLevelName);
+		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+	}
+
+    private void Update()
+    {
+    }
+}
